b16efaec43f77ec36ad7b94fbb346be639ddad96
leaf <zhangqp5@mail3.sysu.edu.cn>
2022-12-09 16:58:40 +0800

    feat:init plum backview

--- src/pages/backView/index.tsx
+++ src/pages/backView/index.tsx
@@ -18,27 +18,48 @@ const BackView = () => {
   };
 
   const init = (ctx: CanvasRenderingContext2D) => {
-    ctx.strokeStyle = '#000';
-    const branch = {
+    ctx.strokeStyle = 'rgba(0, 0, 0, 0.1)';
+    step(ctx, {
       start: {x: WIDTH / 2, y: HEIGHT},
-      length: 100,
+      length: 2,
       theta: -Math.PI / 2,
-    }
-    const end = getEndPoint(branch)
-    drawBranch(ctx, branch);
-    const leftBranch = {
-      start: end,
-      length: 100,
-      theta: branch.theta - 0.1
-    }
-    drawBranch(ctx, leftBranch);
-    const rightBranch = {
-      start: end,
-      length: 100,
-      theta: branch.theta + 0.1
-    }
-    drawBranch(ctx, rightBranch);
+    })
   }
+  const pendingTasks: Function[] = [];
+  const step = (ctx: CanvasRenderingContext2D, b: Branch, depth=0) => {
+    const end = getEndPoint(b)
+    drawBranch(ctx, b);
+    if (depth < 4 || Math.random() < 0.5) {
+      pendingTasks.push(() => step(ctx, {
+        start: end,
+        length: b.length + (Math.random() * 4 - 2),
+        theta: b.theta - 0.4 * Math.random()
+      }, depth + 1))
+    }
+    if (depth < 4 || Math.random() < 0.5) {
+      pendingTasks.push(() => step(ctx, {
+        start: end,
+        length: b.length + (Math.random() * 4 - 2),
+        theta: b.theta + 0.4 * Math.random()
+      }, depth + 1));
+    }
+  }
+  const frame = () => {
+    const tasks = [...pendingTasks];
+    pendingTasks.length = 0;
+    tasks.forEach(fn => fn())
+  }
+  let frameCount = 0;
+  function startFrame() {
+    requestAnimationFrame(() => {
+      frameCount = frameCount + 1
+      if (frameCount % 10 === 0) {
+        frame();
+      }
+      startFrame()
+    })
+  }
+  startFrame()
   const lineTo = (ctx: CanvasRenderingContext2D, p1: Point, p2: Point) => {
     ctx.beginPath()
     ctx.moveTo(p1.x, p1.y)
